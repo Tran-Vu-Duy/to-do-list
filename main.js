d50c15c36d55db386b925e6c8fc3820727abb7f5
Tran-Vu-Duy <tranvud528@gmail.com>
2024-03-06 14:02:16 +0700

    delete task

--- main.js
+++ main.js
@@ -4,9 +4,44 @@ const taskList = document.getElementById('taskList');
 function addTask(){
     const taskText = addinput.value.trim();
     if(taskText!==''){
-        const li = document.createElement('li');
-        li.textContent = taskText;
-        taskList.appendChild(li);
+        const wraper = document.createElement('div');
+        wraper.classList.add("wraper");
+        const items = document.createElement('li');
+        items.classList.add("items");
+        items.textContent = taskText;
+        taskList.appendChild(items);
         addinput.value='';
+
+        items.addEventListener('click',completeTask);
+        
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add("deletebtn");
+        deleteBtn.textContent = "DELETE";
+        deleteBtn.addEventListener('click',deleteTask);
+        wraper.appendChild(items);
+        wraper.appendChild(deleteBtn);
+        taskList.appendChild(wraper);
+        saveTask();
+
+    }
+}
+loadTaskToStorage(); 
+
+function completeTask(event){
+    const task  = event.target;
+    task.classList.toggle('completed');
+}
+
+
+function deleteTask(event){
+    const task = event.target.parentElement;
+    taskList.removeChild(task);
+}
+
+function saveTask(){
+    const tasks= [];
+    const taskItems = taskList.getElementsByTagName('li');
+    for(let i =0 ; i< taskItems.length ; i++){
+        tasks.push(taskItems[i].textContent);
     }
 }
